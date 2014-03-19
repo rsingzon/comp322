70f4f994536626231298cd3351449b57f9d088d5
Ryan Singzon <rcsingzon@gmail.com>
2014-03-19 17:33:44 -0400

    Adding destructor

--- a3/wikiClassesStudent.cpp
+++ a3/wikiClassesStudent.cpp
@@ -18,11 +18,77 @@
 //Constructor for graph class using vertices and edges
 Graph::Graph(list<Edge> lst, int num_vertices){
 
+	//Iterate through vertices and add adjacent vertices to each list
+	for(int vertex = 1; vertex <= num_vertices; vertex++){
+
+		//Create a list of edges for each vertex
+		list<Edge> *edgeList = new list<Edge>;
+
+		//Create a list of the vertices which have already been added
+		vector<int> *existingVertices = new vector<int>;
+
+		//If the origin or destination matches the current vertex, add the edge to list
+		for(Edge edge: lst){
+			if(edge.origin == vertex || edge.destination == vertex){
+				int vertexIndex = edge.origin == vertex ? edge.destination : edge.origin;
+				bool vertexExists = false;
+
+				//Check if the vertex has already been added to the list
+				for (int x : *existingVertices){
+					if(x == vertexIndex) vertexExists = true;
+				}
+
+				//If the vertex does not exist, add it to list
+				if(!vertexExists){
+					existingVertices->push_back(vertexIndex);
+					edgeList->push_back(edge);
+				}
+			}
+		}
+
+		//Add the list to the adjacency list in the graph
+		adj_list.push_back(*edgeList);
+	}
 }
 
 //Constructor for graph class using an input file
+/***
+** FIX THIS
+**/
 Graph::Graph(ifstream& in_file){
+	
+	list<Edge> graph;
 
+	int index = 0;
+	int value;
+	Edge *newEdge;
+
+	//Iterate through all values in the file
+	while (in_file >> value){
+
+		//Assign first value in line as origin
+		if (index == 0){
+			newEdge = new Edge;
+			newEdge->origin = value;
+		}
+
+		//Assign second value as destination
+		else if (index == 1){
+			newEdge->destination = value;
+		}
+
+		//Assign third value to weight and push edge into list
+		else{
+			newEdge->weight = value;
+			graph.push_back(*newEdge);
+			index = -1;
+		}
+
+		index++;
+	}
+
+	//The adjacency list stores lists of edges that include the
+	//current index of the list
 }
 
 //Destructor for graph object
