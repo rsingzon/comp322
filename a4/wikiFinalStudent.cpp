134f8b23eaf2b970a8c4d165424a103c71fe49e1
Ryan Singzon <rcsingzon@gmail.com>
2014-04-04 23:44:12 -0400

    Added method stubs

--- a4/wikiFinalStudent.cpp
+++ a4/wikiFinalStudent.cpp
@@ -10,19 +10,27 @@
 #include "wikiFinal.h"
 
 Graph::Edge sampleEdge(list<Graph::Edge> lst){
+	Graph::Edge edge;
 
+	return edge;
 }
 
 map<int, int> random_walks(int start_node){
+	map<int, int> walk;
 
+	return walk;
 }
 
 list<int> breadth_first_search(int start_node, int number_nodes){
+	list<int> nodeList;
 
+	return nodeList;
 }
 
 list<int> spanning_tree(int start_node, int number_nodes){
+	list<int> spanningTree;
 
+	return spanningTree;
 }
 
 //Destructor for the Graph class
